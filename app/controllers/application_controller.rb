e3d10fc7adc8e4c393b4be2b172a16379b38e358
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-24 11:34:23 +0900

    [Update]ユーザネイムでのログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+	# クロスサイトリクエストフォージェリ (CSRF)への対応策コード
 	protect_from_forgery with: :exception
 
 	before_action :authenticate_user!
+	# ユーザネイムでのログイン機能
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+	# ユーザネイムでのログイン機能
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end	
 end
