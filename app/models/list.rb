227eac832cb0a91df6ae9500d9c9967b4df730a1
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-24 15:32:38 +0900

    [Update]アソシエーションの設定

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+	belogs_to :user
+
+	validates :title, length: { in: 1..255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :list, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 20 }
+
 end
